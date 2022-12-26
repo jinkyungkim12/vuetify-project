27500953d2e67f419755591b4f7623e56d7e77fc
jinkyungkim12 <skwlsrud3@naver.com>
2022-12-26 17:17:35 +0900

    up

--- src/views/test/test3.vue
+++ src/views/test/test3.vue
@@ -136,7 +136,7 @@
           </v-card>
         </v-container>
         <v-card-title>
-          Card3
+          Card 3
         </v-card-title>
         <v-container>
           <v-row justify="space-around">
@@ -205,6 +205,56 @@
             </v-card>
           </v-row>
         </v-container>
+        <v-card-title>
+          Card 4
+        </v-card-title>
+        <v-container>
+          <v-card
+            class="mx-auto"
+            max-width="344"
+          >
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title>
+              Top western road trips
+            </v-card-title>
+
+            <v-card-subtitle>
+              1,000 miles of wonder
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn
+                color="orange lighten-2"
+                text
+              >
+                Explore
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn
+                icon
+                @click="show = !show"
+              >
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                  I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-container>
     </v-card>
 </template>
 <script>
@@ -233,6 +283,7 @@ export default {
           color: 'deep-purple lighten-1',
         },
       ],
+      show: false,
     }),
 
     methods: {
